95ca72e07fcdf0685f05f0432188c26bd5ca0623
eangulee <mbtjay-2004@qq.com>
2019-06-13 19:29:54 +0800

    fixed bug

--- helloworld/helloworld/Shaderh.cpp
+++ helloworld/helloworld/Shaderh.cpp
@@ -92,7 +92,7 @@ void Shader::use()
 
 void Shader::setBool(const std::string& name, bool value) const
 {
-	//glUniform1i(glGetUniformLocation(ID, name.c_str), (int)value);
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
 }
 
 void Shader::setInt(const std::string& name, int value) const
@@ -102,7 +102,7 @@ void Shader::setInt(const std::string& name, int value) const
 
 void Shader::setFloat(const std::string& name, float value) const
 {
-	//glUniform1f(glGetUniformLocation(ID, name.c_str), value);
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 }
 
 Shader::~Shader()
